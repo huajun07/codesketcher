cbe2901045b2ff3b66f3a9a2f48d2d47ae2de0a8
huajun07 <teowhuajun07@gmail.com>
2023-05-24 14:02:26 +0800

    feat: navbar / fix: eslint config

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { theme } from 'theme'
+import NavBar from 'components/NavBar'
+
 import { Router } from './Router'
 
 export const App = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <NavBar />
       <Router />
     </ChakraProvider>
   )
---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,77 @@
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import {
+  Avatar,
+  Box,
+  Button,
+  Center,
+  Flex,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Stack,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react'
+
+export default function NavBar() {
+  const { colorMode, toggleColorMode } = useColorMode()
+  return (
+    <>
+      <Box bg={useColorModeValue('blue.100', 'blue.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Heading color={useColorModeValue('blue.700', 'blue.300')}>
+            CodeSketcher
+          </Heading>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button
+                onClick={toggleColorMode}
+                bg={useColorModeValue('blue.100', 'blue.900')}
+                _hover={{ bg: useColorModeValue('blue.200', 'blue.800') }}
+              >
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}
+                >
+                  <Avatar
+                    size={'sm'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                  />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={'2xl'}
+                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  )
+}
