3e1aa85c73ae2977a39d4ae490b2ed6d9fdc9d83
Teow Hua Jun <teowhuajun07@gmail.com>
2023-05-24 17:43:09 +0800

    feat: code ide (#5)

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
 import { theme } from 'theme'
-import NavBar from 'components/NavBar'
+import { NavBar } from 'components'
 
 import { Router } from './Router'
 
--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -1,3 +1,20 @@
+import { Box, Flex } from '@chakra-ui/react'
+
+import { CodeIDE } from 'components'
+
 export const Main = () => {
-  return <div>Sample Page</div>
+  return (
+    <>
+      <Flex>
+        <Box
+          w="500px"
+          overflowX="scroll"
+          height="calc(100vh - 64px)"
+          borderRightWidth="1px"
+        >
+          <CodeIDE />
+        </Box>
+      </Flex>
+    </>
+  )
 }
