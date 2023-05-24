c9e70f39687c6913fc511f62239b492962759ff0
An Jun <limanjun99@gmail.com>
2023-05-24 23:49:30 +0800

    setup serverless for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import pinoHttp from 'pino-http'
+import serverlessExpress from '@vendia/serverless-express'
 import config from './config'
 import logger from './logger'
 import docsRouter from './docs/docs.route'
@@ -18,6 +19,10 @@ app.use(bodyParser.json())
 app.use(docsRouter)
 app.use(executeRouter)
 
-app.listen(config.get('port'), () => {
-	logger.info(`App started on port ${config.get('port')}`)
-})
+if (config.get('env') === 'local') {
+	app.listen(config.get('port'), () => {
+		logger.info(`App started on port ${config.get('port')}`)
+	})
+}
+
+module.exports.handler = serverlessExpress({ app })
