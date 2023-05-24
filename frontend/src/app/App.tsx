bfac93a9901369f247449b7a9ca0948c3ee61ad5
Teow Hua Jun <teowhuajun07@gmail.com>
2023-05-24 12:03:52 +0800

    chore: Frontend Chakra Template (#2)
    * chore: frontend chakra template
    
    * fix: react-scripts error with ts5

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { Router } from './Router'
+
+export const App = () => {
+  return (
+    <ChakraProvider>
+      <Router />
+    </ChakraProvider>
+  )
+}
