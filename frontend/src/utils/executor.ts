a45c9781d187feb90399fe9d22f15df52b81b7fa
huajun07 <teowhuajun07@gmail.com>
2023-05-26 19:03:33 +0800

    chore: format files

--- frontend/src/utils/executor.ts
+++ frontend/src/utils/executor.ts
@@ -1,20 +1,23 @@
 import axios from 'axios'
 
 interface instruction {
-    line_number: number
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    variable_changes: Record<string, any>
-  }
+  line_number: number
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  variable_changes: Record<string, any>
+}
 
-const getInstructions = async(code: string): Promise<instruction[]> => {
-    try{
-        const res = await axios.post(process.env.REACT_APP_EXECUTOR_ENDPOINT + '/execute', {
-            code
-        })
-        return res.data?.data || [] as instruction[]
-    }catch(err){
-        return []
-    }
+const getInstructions = async (code: string): Promise<instruction[]> => {
+  try {
+    const res = await axios.post(
+      process.env.REACT_APP_EXECUTOR_ENDPOINT + '/execute',
+      {
+        code,
+      },
+    )
+    return res.data?.data || ([] as instruction[])
+  }catch(err){
+    return []
+  }
 }
 
 export { getInstructions }
