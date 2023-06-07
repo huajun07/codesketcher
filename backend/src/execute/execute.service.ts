bc894061abe9a61f6f39b3924f9f0d47f3a7d7ff
An Jun <limanjun99@gmail.com>
2023-06-07 14:05:17 +0800

    handle AWS lambda errors

--- backend/src/execute/execute.service.ts
+++ backend/src/execute/execute.service.ts
@@ -1,20 +1,32 @@
-import { InvokeCommand, Lambda } from '@aws-sdk/client-lambda'
 import config from '../config'
-
-const client = new Lambda({ region: config.get('executorRegion') })
+import logger from '../logger'
+import client from '../aws/lambdaClient'
 
 export default async function execute(payload: {
 	code: string
-	input: string | undefined
+	input?: string
 }) {
-	const command = new InvokeCommand({
-		FunctionName: config.get('executorName'),
-		Payload: Buffer.from(JSON.stringify(payload)),
-	})
-	const { Payload } = await client.send(command)
-	if (Payload === undefined) {
-		return null
+	const result = await client.execute(config.get('executorName'), payload)
+	if ('errorMessage' in result) {
+		// detect if AWS Lambda threw an error
+		const errorMessage = result.errorMessage as string
+		if (new RegExp('Task timed out after [.0-9]* seconds').test(errorMessage)) {
+			return {
+				executed: true,
+				error: 'Execution timed out (code executed for >1 second)',
+			}
+		}
+
+		// if the error is not a timeout, then likely something went wrong
+		// that requires investigation and fixing
+		logger.error(
+			{ awsLambdaError: result },
+			'Unexpected error occured during executor lambda invocation'
+		)
+		return {
+			executed: false,
+			error: 'An unexpected error has occured',
+		}
 	}
-	const result = JSON.parse(Buffer.from(Payload).toString())
 	return result
 }
