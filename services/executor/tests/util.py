79c8d8e87ec3efadb90da9dfc73446f1c3d1e696
An Jun <limanjun99@gmail.com>
2023-06-07 12:25:54 +0800

    add tests

---
+++ services/executor/tests/util.py
@@ -0,0 +1,13 @@
+import re
+
+
+class MatchesRegex:
+    # This is a utility class to make testing easier.
+    # With this class, you can do:
+    # assert x == { "key": MatchesRegex("regex") }
+
+    def __init__(self, regex):
+        self.regex = re.compile(regex)
+
+    def __eq__(self, other):
+        return isinstance(other, str) and bool(self.regex.match(other))
