2bd3541dc53c4e850988f1d28e7799fecf06649a
Teow Hua Jun <teowhuajun07@gmail.com>
2023-06-21 19:10:18 +0800

    feat: Backend auth and CRUD (#50)
    * feat: added crud ops and auth for db
    
    * chore: add dummy google client envar
    
    * chore: setup nvm
    
    * chore: update routes

--- backend/src/user/user.route.ts
+++ backend/src/user/user.route.ts
@@ -1,20 +1,79 @@
 import express from 'express'
+import { addCode, deleteCode, getAllCodes, updateCode } from './user.service'
 import { celebrate, Joi, Segments } from 'celebrate'
-import { getAllCodes } from './user.service'
 
 const router = express()
 
-router.get(
-	'/:uid/codes',
+export interface TypedRequest<P, T> extends Express.Request {
+	body: T
+	params: P
+}
+
+interface CodeData {
+	code: string
+	input?: string
+}
+
+interface CodeName {
+	codename?: string
+}
+
+router.get('/codes', async (_req, res) => {
+	const { uid } = res.locals
+	const response = await getAllCodes(uid)
+	res.status(200).json(response)
+})
+
+router.post(
+	'/codes/:codename',
 	celebrate({
-		[Segments.PARAMS]: Joi.object({
-			uid: Joi.string().required(),
+		[Segments.PARAMS]: Joi.object().keys({
+			codename: Joi.string().required(),
+		}),
+		[Segments.BODY]: Joi.object().keys({
+			code: Joi.string().required(),
+			input: Joi.string(),
 		}),
 	}),
-	async (req, res) => {
-		const { uid } = req.params
-		const response = await getAllCodes(uid)
+	async (req: TypedRequest<CodeName, CodeData>, res) => {
+		const { uid } = res.locals
+		const codename = req.params.codename as string
+		const { code, input } = req.body
+		const response = await addCode(uid, codename, code, input)
+		res.status(201).json(response)
+	}
+)
+router.put(
+	'/codes/:codename',
+	celebrate({
+		[Segments.PARAMS]: Joi.object().keys({
+			codename: Joi.string().required(),
+		}),
+		[Segments.BODY]: Joi.object().keys({
+			code: Joi.string().required(),
+			input: Joi.string(),
+		}),
+	}),
+	async (req: TypedRequest<CodeName, CodeData>, res) => {
+		const { uid } = res.locals
+		const codename = req.params.codename as string
+		const { code, input } = req.body
+		const response = await updateCode(uid, codename, code, input)
 		res.status(200).json(response)
+	}
+)
+router.delete(
+	'/codes/:codename',
+	celebrate({
+		[Segments.PARAMS]: Joi.object().keys({
+			codename: Joi.string().required(),
+		}),
+	}),
+	async (req: TypedRequest<CodeName, undefined>, res) => {
+		const { uid } = res.locals
+		const codename = req.params.codename as string
+		await deleteCode(uid, codename)
+		res.status(200).json({ message: 'Successful Deletion' })
 	}
 )
 
