2bfd218092fe7adb595a607ed477714533a424eb
huajun07 <teowhuajun07@gmail.com>
2023-07-11 00:22:17 +0800

    fix: deattach local volume from non-dev docker-compose

--- scripts/up.sh
+++ scripts/up.sh
@@ -4,4 +4,4 @@ set -e # exit after first failed command
 
 export GOOGLE_CLIENT_ID=$(aws ssm get-parameter --name /codesketcher-dev/google/client-id --output text --query "Parameter.Value")
 
-docker-compose -f docker-compose.yml up --build
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
