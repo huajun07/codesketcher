72b82016e7965e65472490196b6be2ac8029965a
huajun07 <teowhuajun07@gmail.com>
2023-07-11 00:27:26 +0800

    fix: install relevant dependencies in up.sh

--- scripts/up.sh
+++ scripts/up.sh
@@ -2,6 +2,14 @@
 
 set -e # exit after first failed command
 
+cd backend
+npm ci
+npm run build
+
+cd ../frontend
+npm ci --force
+
+cd ../
 export GOOGLE_CLIENT_ID=$(aws ssm get-parameter --name /codesketcher-dev/google/client-id --output text --query "Parameter.Value")
 
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
