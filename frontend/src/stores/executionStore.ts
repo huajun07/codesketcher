1af876917b63552484e9ff4214b30d05ec598670
limanjun99 <30954848+limanjun99@users.noreply.github.com>
2023-07-14 08:37:08 +0800

    Feat/filter steps (#79)
    * feat: filter steps
    
    * fix: frontend build should not specify environment within npm command
    
    * fix: incorrect filter steps
    
    * fix: breakpoints should reset on file change
    
    * fix: breakpoints should not reset on file save

--- frontend/src/stores/executionStore.ts
+++ frontend/src/stores/executionStore.ts
@@ -10,10 +10,13 @@ interface dataVal {
 interface ExecutionState {
   currentStep: number
   setStep: (step: number) => void
+  rawInstructions: instruction[]
   instructions: instruction[]
   setInstructions: (instructions: instruction[]) => void
   data: dataVal[]
   allVariableNames: string[]
+  selectedLineNumbers: number[]
+  updateSelectedLineNumbers: (lineNumbers: number[]) => void
 }
 
 export const useExecutionStore = create<ExecutionState>((set, get) => ({
@@ -37,6 +40,7 @@ export const useExecutionStore = create<ExecutionState>((set, get) => ({
     }
     set({ currentStep: step, data: newData })
   },
+  rawInstructions: [],
   instructions: [],
   setInstructions: (instructions: instruction[]) => {
     const allVariableNames = new Set<string>()
@@ -52,11 +56,68 @@ export const useExecutionStore = create<ExecutionState>((set, get) => ({
     }
     set({
       currentStep: 0,
-      instructions,
+      rawInstructions: instructions,
+      instructions: processInstructions(
+        instructions,
+        get().selectedLineNumbers,
+      ),
       data: [],
       allVariableNames: [...allVariableNames],
     })
   },
   data: [],
   allVariableNames: [],
+  selectedLineNumbers: [],
+  updateSelectedLineNumbers: (lineNumbers: number[]) => {
+    let unchanged = true
+    if (lineNumbers.length !== get().selectedLineNumbers.length)
+      unchanged = false
+    else {
+      for (let i = 0; i < lineNumbers.length; i++) {
+        if (lineNumbers[i] !== get().selectedLineNumbers[i]) unchanged = false
+      }
+    }
+    if (unchanged) return
+    set({
+      selectedLineNumbers: lineNumbers,
+      instructions: processInstructions(get().rawInstructions, lineNumbers),
+      data: [],
+      currentStep: 0,
+    })
+  },
 }))
+
+function processInstructions(
+  rawInstructions: instruction[],
+  lineNumbers: number[],
+): instruction[] {
+  const newInstructions: instruction[] = []
+  const lineNumberSet = new Set(lineNumbers)
+  let rangeStart = 0
+  for (let i = 0; i < rawInstructions.length; i++) {
+    if (
+      lineNumberSet.size > 0 &&
+      !lineNumberSet.has(rawInstructions[i].line_number)
+    )
+      continue
+    const instruction = {
+      line_number: rawInstructions[i].line_number,
+      function_scope: rawInstructions[i].function_scope,
+      global_variable_changes: {},
+      local_variable_changes: {},
+    }
+    for (let j = rangeStart; j <= i; j++) {
+      Object.assign(
+        instruction.global_variable_changes,
+        rawInstructions[j].global_variable_changes,
+      )
+      Object.assign(
+        instruction.local_variable_changes,
+        rawInstructions[j].local_variable_changes,
+      )
+    }
+    rangeStart = i + 1
+    newInstructions.push(instruction)
+  }
+  return newInstructions
+}
