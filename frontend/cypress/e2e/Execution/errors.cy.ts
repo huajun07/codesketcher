92f32e44e103f9eb2827870cc9b4f3b9a6fbad83
huajun07 <teowhuajun07@gmail.com>
2023-07-22 17:17:06 +0800

    feat: errors check

---
+++ frontend/cypress/e2e/Execution/errors.cy.ts
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+// import { inputCode } from "../../support/constants"
+
+describe('Error Checks', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+
+  it('No Code', () => {
+    cy.contains('Run').click()
+    cy.contains('An Error Has Occured')
+  })
+
+  // Unable to test as local lambda crashes
+  // it('No input', () => {
+  //   cy.typeIDE(inputCode, 0)
+  //   cy.contains('Run').click()
+  //   cy.contains('An Error Has Occured')
+  //   cy.get('button[aria-label="Play/Pause"]').should('be.disabled')
+  // })
+})
